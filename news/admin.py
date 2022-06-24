d6719cb00eb068e2454260d2034c7a3d99da5451
Abai <abai.org@gmail.com>
2022-06-24 10:15:22 +0600

    models design and admin configs

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post, Image
 
-# Register your models here.
+
+class ImagesStacked(admin.StackedInline):
+    model = Image
+    extra = 1
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp', 'id')
+    search_fields = ('title',)
+    inlines = [ImagesStacked]
+
--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_query_name='images')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='uploads')
